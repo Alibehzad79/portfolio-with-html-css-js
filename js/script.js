53a6def8164968c32f0c211e87ae6ad4c3a10764
AliBehzad <61663367+Alibehzad79@users.noreply.github.com>
2022-06-15 02:54:19 +0430

    add about section and fix bugs

--- js/script.js
+++ js/script.js
@@ -28,3 +28,49 @@ function closeModal() {
     document.body.style.overflow = 'auto';
     document.body.style.userSelect = 'auto';
 }
+
+function navStiky() {
+    let navbar = document.querySelector('#navbar');
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 0) {
+            navbar.classList.add('stiky')
+        } else {
+            navbar.classList.remove('stiky')
+        }
+    })
+}
+
+navStiky();
+
+// function navItemActive() {
+//     let portfolio = document.querySelector('#portfolio');
+//     if (window.scrollIntoView() == portfolio) {
+//         document.querySelector('#navbar').querySelector('#portfolio').classList.add('active')
+//     }
+// }
+
+// navItemActive();
+
+function formAnimation() {
+    let formItems = document.querySelectorAll('.form-item');
+    formItems.forEach(item => {
+        item.addEventListener('click', () => {
+            item.querySelector('label').classList.add('active');
+            let itemvalue = item.querySelector('.form-input').value;
+            if (itemvalue != '') {
+                item.querySelector('label').classList.add('active');
+            }
+        })
+    })
+    formItems.forEach(item => {
+        item.querySelector('label').classList.add('active');
+        let itemvalue = item.querySelector('.form-input').value;
+        if (itemvalue != '' && itemvalue != null && itemvalue != ' ') {
+            item.querySelector('label').classList.add('active');
+        } else {
+            item.querySelector('label').classList.remove('active');
+        }
+    })
+}
+
+formAnimation();
